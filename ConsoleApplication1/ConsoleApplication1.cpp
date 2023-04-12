3bc5d7cd41732adb1873ffd2ff6e8bc9ff29ecd0
user <user@WIN-4HU5T7LM44J>
2023-04-12 19:25:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	cout << "I love C++!\n";
+	cout << "\n";
+	cout << "\t  I love C++!\n";
+	cout << "\n";
+	cout << "\t\t I love C++!\n";
+	cout << "\n";
+	cout << "\t\t\t I love C++! \n";
+	cout << "\n";
+	cout << "\t\t\t\t I love C++! \n";
+}
+
+
+
+// \n - new line переход на новую строку
+// \t - табуляция (1-8 пробелов, для выравнивания)
+// \" - вывод на экран консоли "
+// \\ - вывод на экран консоли \
